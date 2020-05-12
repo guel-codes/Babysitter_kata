a2765201e89b881b44ca031539f0476c7800e3f8
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-12 19:35:16 -0400

    refactored to military time

--- babysitter.py
+++ babysitter.py
@@ -1,5 +1,5 @@
 def hours_worked(start,end):
-    if start < 5:
-        start = 5
+    if start < 17:
+        start = 17
     return (end - start)
     
--- test_babysitter.py
+++ test_babysitter.py
@@ -1,8 +1,8 @@
 from babysitter import *
 
 def test_hours_worked():
-    assert hours_worked(5,6) == 1
+    assert hours_worked(17,18) == 1
 
 def test_start_time():
-    assert hours_worked(4,6) == 1
+    assert hours_worked(16,18) == 1
     
