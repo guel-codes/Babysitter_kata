4b6227c28b929b5b1d684afe52d6e30ef25f1753
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-16 10:13:48 -0400

    checking tests

--- test_babysitter.py
+++ test_babysitter.py
@@ -1,10 +1,15 @@
 from babysitter import *
 
 def test_hours_worked():
-    assert hours_worked(17,18) == 1
+    start_time = 17 
+    end_time = 18
+    assert hours_worked(start_time,end_time) == 1
 
 def test_start_time():
-    assert hours_worked(16,18) == 1
+    #tests start time before 5 should change 16 to 17
+    start_time = 16 
+    end_time = 18
+    assert hours_worked(start_time,end_time) == 1
 
 def test_end_time():
     assert hours_worked(23,0) == 1
